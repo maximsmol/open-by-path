97e40df8ea2f608a838e6a33c2431cf735b67f67
maximsmol <1472826+maximsmol@users.noreply.github.com>
2022-02-18 15:20:35 -0800

    feat: first working implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,12 @@
 # Change Log
 
-All notable changes to the "open-by-path" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
 
+## [0.1.0] - 2022-02-18
+
 - Initial release
+
+[unreleased]: https://github.com/maximsmol/open-by-path/compare/v0.1.0...HEAD
+[0.1.0]: https://github.com/maximsmol/open-by-path/releases/tag/v0.1.0
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,110 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as fs from "fs/promises";
+import * as path from "path";
+import * as os from "os";
+
+import Fuse from "fuse.js";
+
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "open-by-path" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand("open-by-path.open-by-path", () => {
+      const qp = vscode.window.createQuickPick();
+      qp.title = "Open by Path";
+      qp.placeholder = "Path to open…";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('open-by-path.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Open by Path!');
-	});
+      const homedir = os.homedir();
 
-	context.subscriptions.push(disposable);
+      let rel: string | undefined;
+      const updateItems = async (x: string) => {
+        const p = path.parse(x);
+        x = x.slice(p.root.length);
+
+        const absolute = p.root !== "";
+
+        rel = undefined;
+        if (!absolute && x.startsWith("~")) {
+          rel = homedir;
+          x = x.slice(1);
+        }
+        if (rel === undefined) {
+          rel = vscode.window.activeTextEditor?.document.fileName;
+          if (rel !== undefined) rel = path.dirname(rel);
+        }
+        if (rel === undefined)
+          rel = (vscode.workspace.workspaceFolders ?? [])[0]?.uri.fsPath;
+        if (rel === undefined) rel = process.cwd();
+
+        if (!absolute && rel !== undefined)
+          // Can't use `path` since `path.join("a/b/", "./.")` is `"a/b"` (strips trailing `/`)
+          x = rel + path.sep + x;
+
+        qp.title = rel.replace(homedir, "~");
+
+        const parts = x.split(path.sep);
+
+        let filter = parts.length === 0 ? "" : parts[parts.length - 1];
+        filter = filter.toLocaleLowerCase();
+
+        for (let i = 1; i <= parts.length; ++i) {
+          let subpath = parts.slice(0, -i).join(path.sep);
+
+          if (absolute) subpath = p.root + subpath;
+
+          let files;
+          try {
+            files = await fs.readdir(subpath, {
+              withFileTypes: true,
+            });
+          } catch (e) {
+            continue;
+          }
+
+          let sorted = files;
+          if (filter !== "") {
+            const fuse = new Fuse(files, {
+              keys: ["name"],
+              includeScore: true,
+              threshold: 1.0,
+              ignoreLocation: true,
+            });
+            sorted = fuse.search(filter).map((x) => x.item);
+          }
+
+          qp.items = sorted.map((x) => ({
+            label: x.isDirectory() ? `${x.name}${path.sep}` : x.name,
+            alwaysShow: true,
+          }));
+          return;
+        }
+
+        qp.items = [];
+      };
+      qp.onDidChangeValue(updateItems);
+      qp.onDidAccept(() => {
+        const label = qp.selectedItems[0].label;
+        let val = qp.value;
+
+        if (val.startsWith("~")) val = val.slice(2);
+
+        const filterStart = val.lastIndexOf(path.sep) + 1;
+        let newPath = val.slice(0, filterStart) + label;
+
+        if (!label.endsWith(path.sep)) {
+          if (rel !== undefined) newPath = path.join(rel, newPath);
+          vscode.workspace
+            .openTextDocument(vscode.Uri.file(newPath))
+            .then(vscode.window.showTextDocument);
+          qp.dispose();
+          return;
+        }
+
+        qp.value = newPath;
+      });
+      updateItems("");
+      qp.show();
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
